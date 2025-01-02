62fe4c4b1c2c3da9064b5ff38518c2d20098f87f
“Viktor <viktorengel@hotmail.com>
2025-01-02 00:35:21 -0500

    Rutas 2 mensajes

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+/* Route::get('/', function () {
     return view('welcome');
+}); */
+
+Route::get('/', function(){
+    return "Hola amigos";
 });
 
+Route::get('/posts', function(){
+    return "Aquí van los post";
+});

65e5dec36b51cd4b2c8d489c069eb0166510539a
“Viktor <viktorengel@hotmail.com>
2025-01-03 22:49:18 -0500

    x-alert x-slot

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,3 +1,3 @@
 <div class="p-4 mb-4 text-sm text-blue-800 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400" role="alert">
-    <span class="font-medium">Info alert!</span> Bienvenido a la página principal
+    <span class="font-medium">{{ $title }}</span> {{ $slot }}
 </div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,7 +10,11 @@
 <body>
     <div class="max-w-4xl mx-auto px-4">
         {{-- <h1>Bienvenido a la página principal</h1> --}}
-        <x-alert>  
+        <x-alert>
+            <x-slot name="title">
+                Titulo de la alerta
+            </x-slot>
+            Contenido de la alerta
         </x-alert>
     </div>
 </body>

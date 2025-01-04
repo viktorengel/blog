e186bd224311f1cb9de5e5532cc8e0bb6ca7ce71
“Viktor <viktorengel@hotmail.com>
2025-01-03 22:24:45 -0500

    Component alert

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,8 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel 11</title>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
-    <h1>Bienvenido a la página principal</h1>
+    <div class="max-w-4xl mx-auto px-4">
+        {{-- <h1>Bienvenido a la página principal</h1> --}}
+        <x-alert>  
+        </x-alert>
+    </div>
 </body>
 </html>
